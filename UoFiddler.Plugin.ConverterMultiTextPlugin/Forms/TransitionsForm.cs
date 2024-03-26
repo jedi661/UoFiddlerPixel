9b6036e4550f4a6ea7057087a14715fa70a9f654
Jedi66 <Jedi66@gmx.de>
2024-03-26 19:59:21 +0100

    Info

--- UoFiddler.Plugin.ConverterMultiTextPlugin/Forms/TransitionsForm.cs
+++ UoFiddler.Plugin.ConverterMultiTextPlugin/Forms/TransitionsForm.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/***************************************************************************
+ *
+ * $Author: Prapilk
+ * Built-in : Nikodeus
+ * 
+ * "THE WINE-WARE LICENSE"
+ * As long as you retain this notice you can do whatever you want with 
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a Wine in return.
+ *
+ ***************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
