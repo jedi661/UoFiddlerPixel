8f511df2ce22cfd9cdcbb0e927d69935b1f35c04
Jedi66 <Jedi66@gmx.de>
2024-05-17 00:14:43 +0200

    Add TIFUnicode from Prapilk

---
+++ UoFiddler.Controls/Forms/TTFunicodeForm.Designer.cs
@@ -0,0 +1,175 @@
+﻿// /***************************************************************************
+//  *
+//  * $Author: Turley
+//  * Advanced Nikodemus
+//  * 
+//  * \"THE BEER-WINE-WARE LICENSE\"
+//  * As long as you retain this notice you can do whatever you want with 
+//  * this stuff. If we meet some day, and you think this stuff is worth it,
+//  * you can buy me a beer and Wine in return.
+//  *
+//  ***************************************************************************/
+
+namespace UoFiddler.Controls.Forms
+{
+    partial class TTFunicodeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TTFunicodeForm));
+            panel1 = new System.Windows.Forms.Panel();
+            btOpenDir = new System.Windows.Forms.Button();
+            lbformat = new System.Windows.Forms.Label();
+            lbsize = new System.Windows.Forms.Label();
+            comboBoxFontStyle = new System.Windows.Forms.ComboBox();
+            comboBoxFontSize = new System.Windows.Forms.ComboBox();
+            buttonExport = new System.Windows.Forms.Button();
+            buttonImportTTF = new System.Windows.Forms.Button();
+            pictureBoxGlyphPreview = new System.Windows.Forms.PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxGlyphPreview).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            panel1.Controls.Add(btOpenDir);
+            panel1.Controls.Add(lbformat);
+            panel1.Controls.Add(lbsize);
+            panel1.Controls.Add(comboBoxFontStyle);
+            panel1.Controls.Add(comboBoxFontSize);
+            panel1.Controls.Add(buttonExport);
+            panel1.Controls.Add(buttonImportTTF);
+            panel1.Location = new System.Drawing.Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(200, 186);
+            panel1.TabIndex = 0;
+            // 
+            // btOpenDir
+            // 
+            btOpenDir.Location = new System.Drawing.Point(93, 9);
+            btOpenDir.Name = "btOpenDir";
+            btOpenDir.Size = new System.Drawing.Size(65, 23);
+            btOpenDir.TabIndex = 8;
+            btOpenDir.Text = "Open Dir";
+            btOpenDir.UseVisualStyleBackColor = true;
+            btOpenDir.Click += btOpenDir_Click;
+            // 
+            // lbformat
+            // 
+            lbformat.AutoSize = true;
+            lbformat.Location = new System.Drawing.Point(12, 72);
+            lbformat.Name = "lbformat";
+            lbformat.Size = new System.Drawing.Size(43, 15);
+            lbformat.TabIndex = 7;
+            lbformat.Text = "format";
+            // 
+            // lbsize
+            // 
+            lbsize.AutoSize = true;
+            lbsize.Location = new System.Drawing.Point(143, 72);
+            lbsize.Name = "lbsize";
+            lbsize.Size = new System.Drawing.Size(27, 15);
+            lbsize.TabIndex = 6;
+            lbsize.Text = "Size";
+            // 
+            // comboBoxFontStyle
+            // 
+            comboBoxFontStyle.FormattingEnabled = true;
+            comboBoxFontStyle.Location = new System.Drawing.Point(12, 90);
+            comboBoxFontStyle.Name = "comboBoxFontStyle";
+            comboBoxFontStyle.Size = new System.Drawing.Size(121, 23);
+            comboBoxFontStyle.TabIndex = 5;
+            // 
+            // comboBoxFontSize
+            // 
+            comboBoxFontSize.FormattingEnabled = true;
+            comboBoxFontSize.Items.AddRange(new object[] { "2", "4", "6", "8", "10", "12", "14", "16", "18", "20", "22", "24", "26", "28", "30" });
+            comboBoxFontSize.Location = new System.Drawing.Point(143, 90);
+            comboBoxFontSize.Name = "comboBoxFontSize";
+            comboBoxFontSize.Size = new System.Drawing.Size(37, 23);
+            comboBoxFontSize.TabIndex = 4;
+            // 
+            // buttonExport
+            // 
+            buttonExport.Location = new System.Drawing.Point(12, 38);
+            buttonExport.Name = "buttonExport";
+            buttonExport.Size = new System.Drawing.Size(75, 23);
+            buttonExport.TabIndex = 3;
+            buttonExport.Text = "Export";
+            buttonExport.UseVisualStyleBackColor = true;
+            buttonExport.Click += ButtonExport_Click;
+            // 
+            // buttonImportTTF
+            // 
+            buttonImportTTF.Location = new System.Drawing.Point(12, 9);
+            buttonImportTTF.Name = "buttonImportTTF";
+            buttonImportTTF.Size = new System.Drawing.Size(75, 23);
+            buttonImportTTF.TabIndex = 2;
+            buttonImportTTF.Text = "Import TTF";
+            buttonImportTTF.UseVisualStyleBackColor = true;
+            buttonImportTTF.Click += ButtonImportTTF_Click;
+            // 
+            // pictureBoxGlyphPreview
+            // 
+            pictureBoxGlyphPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            pictureBoxGlyphPreview.Location = new System.Drawing.Point(218, 12);
+            pictureBoxGlyphPreview.Name = "pictureBoxGlyphPreview";
+            pictureBoxGlyphPreview.Size = new System.Drawing.Size(958, 186);
+            pictureBoxGlyphPreview.TabIndex = 1;
+            pictureBoxGlyphPreview.TabStop = false;
+            // 
+            // TTFunicodeForm
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1184, 208);
+            Controls.Add(pictureBoxGlyphPreview);
+            Controls.Add(panel1);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            Name = "TTFunicodeForm";
+            Text = "TTF - Unicode Export";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxGlyphPreview).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBoxGlyphPreview;
+        private System.Windows.Forms.ComboBox comboBoxFontStyle;
+        private System.Windows.Forms.ComboBox comboBoxFontSize;
+        private System.Windows.Forms.Button buttonExport;
+        private System.Windows.Forms.Button buttonImportTTF;
+        private System.Windows.Forms.Button btOpenDir;
+        private System.Windows.Forms.Label lbformat;
+        private System.Windows.Forms.Label lbsize;
+    }
+}
---
+++ UoFiddler.Controls/Forms/TTFunicodeForm.cs
@@ -0,0 +1,224 @@
+﻿// /***************************************************************************
+//  *
+//  * $Author: Prapilk
+//  * Advanced Nikodemus
+//  * 
+//  * \"THE BEER-WINE-WARE LICENSE\"
+//  * As long as you retain this notice you can do whatever you want with 
+//  * this stuff. If we meet some day, and you think this stuff is worth it,
+//  * you can buy me a beer and Wine in return.
+//  *
+//  ***************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UoFiddler.Controls.Forms
+{
+    public partial class TTFunicodeForm : Form
+    {        
+        private static TTFunicodeForm currentInstance = null; // Static variable to store the current instance of the shape
+        private string importedTTFFilePath;
+        private float selectedFontSize = 12; // Default font size
+        private FontStyle selectedFontStyle = FontStyle.Regular; // Default font style                                                                 
+        private string lastImportedDirectory; // Variable to store the path of the last imported directory
+
+        public TTFunicodeForm()
+        {
+            InitializeComponent();
+            InitializeControls();
+        }
+
+        #region InitializeControls
+        private void InitializeControls()
+        {
+            // Add different font sizes to the ComboBoxFontSize
+            comboBoxFontSize.Items.AddRange(new object[] { 10, 12, 14, 16, 18, 20 });
+            comboBoxFontSize.SelectedIndex = 1; // Select the default font size
+
+            // Add different font styles to the ComboBoxFontStyle
+            comboBoxFontStyle.Items.AddRange(Enum.GetValues(typeof(FontStyle)).Cast<object>().ToArray());
+            comboBoxFontStyle.SelectedIndex = 0; // Select the default font style
+
+            // Associate the events with the ComboBoxes
+            comboBoxFontSize.SelectedIndexChanged += ComboBoxFontSize_SelectedIndexChanged;
+            comboBoxFontStyle.SelectedIndexChanged += ComboBoxFontStyle_SelectedIndexChanged;
+        }
+        #endregion
+
+        #region TFunicodeForm GetCurrentInstance
+        // Static method to get the current instance of the shape
+        public static TTFunicodeForm GetCurrentInstance()
+        {
+            if (currentInstance == null || currentInstance.IsDisposed)
+            {
+                currentInstance = new TTFunicodeForm();
+            }
+            return currentInstance;
+        }
+        #endregion
+
+        #region ButtonImportTTF
+        private void ButtonImportTTF_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Font files (*.ttf)|*.ttf|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Save the directory of the selected file
+                lastImportedDirectory = Path.GetDirectoryName(openFileDialog.FileName);
+
+                importedTTFFilePath = openFileDialog.FileName;
+                DisplayGlyphPreview(importedTTFFilePath);
+                MessageBox.Show("Import completed.");
+            }
+        }
+        #endregion
+
+        #region ButtonExport
+        private void ButtonExport_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(importedTTFFilePath))
+            {
+                string directoryPath = Path.GetDirectoryName(importedTTFFilePath);
+                string fontFileNameWithoutExtension = Path.GetFileNameWithoutExtension(importedTTFFilePath);
+                try
+                {
+                    ExportGlyphsAsBitmaps(importedTTFFilePath, directoryPath, fontFileNameWithoutExtension);
+                    MessageBox.Show("Export completed successfully!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error during the export of characters in .bmp format: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please import a TTF file first.");
+            }
+        }
+        #endregion
+
+        #region DisplayGlyphPreview
+        private void DisplayGlyphPreview(string ttfFilePath)
+        {
+            var fontCollection = new PrivateFontCollection();
+            fontCollection.AddFontFile(ttfFilePath);
+            var fontFamily = fontCollection.Families[0];
+
+            // Base Unicode text to display
+            string unicodeText = "abcdefghijklm nopqrstuvw xyz A BCD EFG H IJK L M N O PQ R ST U V W X Y Z + */ û1234567890.:,;'" + "(!?)-=éèàçùê â";
+
+            // Create an image containing the rendered text
+            Bitmap bmp = new Bitmap(pictureBoxGlyphPreview.Width, pictureBoxGlyphPreview.Height);
+            using (Graphics graphics = Graphics.FromImage(bmp))
+            {
+                // Clear the image with a white background
+                graphics.Clear(Color.White);
+
+                // Render the text on the image with the selected font size and style
+                using (var font = new Font(fontFamily, selectedFontSize, selectedFontStyle))
+                {
+                    graphics.DrawString(unicodeText, font, Brushes.Black, PointF.Empty);
+                }
+            }
+
+            // Display the image in the PictureBox
+            pictureBoxGlyphPreview.Image = bmp;
+        }
+        #endregion
+
+        #region ExportGlyphsAsBitmaps
+        private void ExportGlyphsAsBitmaps(string ttfFilePath, string outputDirectory, string fontFileNameWithoutExtension)
+        {
+            // Load the TrueType font using PrivateFontCollection
+            using (PrivateFontCollection privateFontCollection = new PrivateFontCollection())
+            {
+                privateFontCollection.AddFontFile(ttfFilePath);
+
+                // Create a Font object from the loaded font
+                using (Font font = new Font(privateFontCollection.Families[0], selectedFontSize, selectedFontStyle))
+                {
+                    // Create an array of standard Unicode characters to export
+                    int startUnicode = 32; // Start of the standard Unicode character range
+                    int endUnicode = 126; // End of the standard Unicode character range
+
+                    // Iterate over each standard Unicode character to export
+                    for (int charCode = startUnicode; charCode <= endUnicode; charCode++)
+                    {
+                        char character = (char)charCode;
+
+                        // Create a Bitmap for each character and draw it
+                        Bitmap bitmap = new Bitmap(1, 1); // Start with a small size
+                        using (Graphics graphics = Graphics.FromImage(bitmap))
+                        {
+                            SizeF size = graphics.MeasureString(character.ToString(), font);
+
+                            // Adjust the size of the bitmap based on the size of the text
+                            bitmap = new Bitmap((int)Math.Ceiling(size.Width), (int)Math.Ceiling(size.Height));
+
+                            // Redraw the text on the resized bitmap
+                            using (Graphics g = Graphics.FromImage(bitmap))
+                            {
+                                g.Clear(Color.White); // White background
+                                g.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+                                g.DrawString(character.ToString(), font, Brushes.Black, new PointF(0, 0));
+                            }
+                        }
+
+                        // Save the Bitmap in BMP format
+                        string bmpFileName = $"{outputDirectory}\\{fontFileNameWithoutExtension}_char_{charCode}.bmp";
+                        bitmap.Save(bmpFileName, ImageFormat.Bmp);
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region ComboBoxFontSize_SelectedIndexChanged
+        private void ComboBoxFontSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedFontSize = Convert.ToSingle(comboBoxFontSize.SelectedItem);
+            // Refresh the glyph preview with the new font size
+            DisplayGlyphPreview(importedTTFFilePath);
+        }
+        #endregion
+
+        #region ComboBoxFontStyle_SelectedIndexChanged
+        private void ComboBoxFontStyle_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Update the selected font style
+            selectedFontStyle = (FontStyle)comboBoxFontStyle.SelectedItem;
+            // Refresh the glyph preview with the new font style
+            DisplayGlyphPreview(importedTTFFilePath);
+        }
+        #endregion
+
+        #region btOpenDir
+        private void btOpenDir_Click(object sender, EventArgs e)
+        {
+            // Check if a directory was imported
+            if (!string.IsNullOrEmpty(lastImportedDirectory))
+            {
+                // Open the directory in Windows Explorer
+                System.Diagnostics.Process.Start("explorer.exe", lastImportedDirectory);
+            }
+            else
+            {
+                MessageBox.Show("Please import a file first.");
+            }
+        }
+        #endregion
+    }
+}
+
