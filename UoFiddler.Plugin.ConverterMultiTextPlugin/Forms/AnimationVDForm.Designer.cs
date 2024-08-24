e0fd8e9c391ab61ddb5d079145a756f9d498317c
Jedi66 <Jedi66@gmx.de>
2024-08-24 14:32:42 +0200

    Image animation management with support for image import from clipboard and load and customizable animation speed.

--- UoFiddler.Plugin.ConverterMultiTextPlugin/Forms/AnimationVDForm.Designer.cs
+++ UoFiddler.Plugin.ConverterMultiTextPlugin/Forms/AnimationVDForm.Designer.cs
@@ -39,19 +39,201 @@ namespace UoFiddler.Plugin.ConverterMultiTextPlugin.Forms
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            pictureBoxAminImage = new System.Windows.Forms.PictureBox();
+            contextMenuStripPictureBox = new System.Windows.Forms.ContextMenuStrip(components);
+            importImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            loadImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            playToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            panel1 = new System.Windows.Forms.Panel();
+            labelSpeed = new System.Windows.Forms.Label();
+            btStopAminID = new System.Windows.Forms.Button();
+            checkBoxLoop = new System.Windows.Forms.CheckBox();
+            trackBarSpeedAmin = new System.Windows.Forms.TrackBar();
+            btPlayAminID = new System.Windows.Forms.Button();
+            btLoadAminID = new System.Windows.Forms.Button();
+            checkedListBoxAminID = new System.Windows.Forms.CheckedListBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxAminImage).BeginInit();
+            contextMenuStripPictureBox.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)trackBarSpeedAmin).BeginInit();
             SuspendLayout();
+            // 
+            // pictureBoxAminImage
+            // 
+            pictureBoxAminImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            pictureBoxAminImage.ContextMenuStrip = contextMenuStripPictureBox;
+            pictureBoxAminImage.Location = new System.Drawing.Point(20, 15);
+            pictureBoxAminImage.Name = "pictureBoxAminImage";
+            pictureBoxAminImage.Size = new System.Drawing.Size(148, 316);
+            pictureBoxAminImage.TabIndex = 0;
+            pictureBoxAminImage.TabStop = false;
+            // 
+            // contextMenuStripPictureBox
+            // 
+            contextMenuStripPictureBox.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { importImageToolStripMenuItem, loadImageToolStripMenuItem, toolStripSeparator1, playToolStripMenuItem, stopToolStripMenuItem });
+            contextMenuStripPictureBox.Name = "contextMenuStripPictureBox";
+            contextMenuStripPictureBox.Size = new System.Drawing.Size(202, 98);
+            // 
+            // importImageToolStripMenuItem
+            // 
+            importImageToolStripMenuItem.Image = Properties.Resources.import;
+            importImageToolStripMenuItem.Name = "importImageToolStripMenuItem";
+            importImageToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            importImageToolStripMenuItem.Text = "Import Image Clipboard";
+            importImageToolStripMenuItem.Click += importImageToolStripMenuItem_Click;
+            // 
+            // loadImageToolStripMenuItem
+            // 
+            loadImageToolStripMenuItem.Image = Properties.Resources.Load;
+            loadImageToolStripMenuItem.Name = "loadImageToolStripMenuItem";
+            loadImageToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            loadImageToolStripMenuItem.Text = "Load Image";
+            loadImageToolStripMenuItem.Click += btLoadAminID_Click;
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new System.Drawing.Size(198, 6);
+            // 
+            // playToolStripMenuItem
+            // 
+            playToolStripMenuItem.Image = Properties.Resources.right;
+            playToolStripMenuItem.Name = "playToolStripMenuItem";
+            playToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            playToolStripMenuItem.Text = "Play";
+            playToolStripMenuItem.Click += btPlayAminID_Click;
+            // 
+            // stopToolStripMenuItem
+            // 
+            stopToolStripMenuItem.Image = Properties.Resources.uomc02;
+            stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            stopToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            stopToolStripMenuItem.Text = "Stop";
+            stopToolStripMenuItem.Click += btStopAminID_Click;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(labelSpeed);
+            panel1.Controls.Add(btStopAminID);
+            panel1.Controls.Add(checkBoxLoop);
+            panel1.Controls.Add(trackBarSpeedAmin);
+            panel1.Controls.Add(btPlayAminID);
+            panel1.Controls.Add(btLoadAminID);
+            panel1.Controls.Add(checkedListBoxAminID);
+            panel1.Controls.Add(pictureBoxAminImage);
+            panel1.Location = new System.Drawing.Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(342, 346);
+            panel1.TabIndex = 1;
+            // 
+            // labelSpeed
+            // 
+            labelSpeed.AutoSize = true;
+            labelSpeed.Location = new System.Drawing.Point(188, 316);
+            labelSpeed.Name = "labelSpeed";
+            labelSpeed.Size = new System.Drawing.Size(39, 15);
+            labelSpeed.TabIndex = 7;
+            labelSpeed.Text = "Speed";
+            // 
+            // btStopAminID
+            // 
+            btStopAminID.Location = new System.Drawing.Point(246, 63);
+            btStopAminID.Name = "btStopAminID";
+            btStopAminID.Size = new System.Drawing.Size(75, 23);
+            btStopAminID.TabIndex = 6;
+            btStopAminID.Text = "Stop";
+            btStopAminID.UseVisualStyleBackColor = true;
+            btStopAminID.Click += btStopAminID_Click;
+            // 
+            // checkBoxLoop
+            // 
+            checkBoxLoop.AutoSize = true;
+            checkBoxLoop.Checked = true;
+            checkBoxLoop.CheckState = System.Windows.Forms.CheckState.Checked;
+            checkBoxLoop.Location = new System.Drawing.Point(188, 261);
+            checkBoxLoop.Name = "checkBoxLoop";
+            checkBoxLoop.Size = new System.Drawing.Size(53, 19);
+            checkBoxLoop.TabIndex = 5;
+            checkBoxLoop.Text = "Loop";
+            checkBoxLoop.UseVisualStyleBackColor = true;
+            // 
+            // trackBarSpeedAmin
+            // 
+            trackBarSpeedAmin.Location = new System.Drawing.Point(188, 286);
+            trackBarSpeedAmin.Maximum = 5;
+            trackBarSpeedAmin.Minimum = 1;
+            trackBarSpeedAmin.Name = "trackBarSpeedAmin";
+            trackBarSpeedAmin.Size = new System.Drawing.Size(104, 45);
+            trackBarSpeedAmin.TabIndex = 4;
+            trackBarSpeedAmin.Value = 3;
+            // 
+            // btPlayAminID
+            // 
+            btPlayAminID.Location = new System.Drawing.Point(246, 39);
+            btPlayAminID.Name = "btPlayAminID";
+            btPlayAminID.Size = new System.Drawing.Size(75, 23);
+            btPlayAminID.TabIndex = 3;
+            btPlayAminID.Text = "Play";
+            btPlayAminID.UseVisualStyleBackColor = true;
+            btPlayAminID.Click += btPlayAminID_Click;
+            // 
+            // btLoadAminID
+            // 
+            btLoadAminID.Location = new System.Drawing.Point(246, 15);
+            btLoadAminID.Name = "btLoadAminID";
+            btLoadAminID.Size = new System.Drawing.Size(75, 23);
+            btLoadAminID.TabIndex = 2;
+            btLoadAminID.Text = "Load";
+            btLoadAminID.UseVisualStyleBackColor = true;
+            btLoadAminID.Click += btLoadAminID_Click;
+            // 
+            // checkedListBoxAminID
+            // 
+            checkedListBoxAminID.FormattingEnabled = true;
+            checkedListBoxAminID.Items.AddRange(new object[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" });
+            checkedListBoxAminID.Location = new System.Drawing.Point(188, 15);
+            checkedListBoxAminID.Name = "checkedListBoxAminID";
+            checkedListBoxAminID.Size = new System.Drawing.Size(39, 184);
+            checkedListBoxAminID.TabIndex = 1;
+            checkedListBoxAminID.ItemCheck += CheckedListBoxAminID_ItemCheck;
             // 
             // AnimationVDForm
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(945, 534);
+            ClientSize = new System.Drawing.Size(376, 372);
+            Controls.Add(panel1);
             FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             Name = "AnimationVDForm";
-            Text = "AnimationVDForm";
+            Text = "Animation Playing";
+            FormClosing += AnimationVDForm_FormClosing;
+            ((System.ComponentModel.ISupportInitialize)pictureBoxAminImage).EndInit();
+            contextMenuStripPictureBox.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)trackBarSpeedAmin).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxAminImage;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btLoadAminID;
+        private System.Windows.Forms.CheckedListBox checkedListBoxAminID;
+        private System.Windows.Forms.Button btPlayAminID;
+        private System.Windows.Forms.CheckBox checkBoxLoop;
+        private System.Windows.Forms.TrackBar trackBarSpeedAmin;
+        private System.Windows.Forms.Button btStopAminID;
+        private System.Windows.Forms.Label labelSpeed;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripPictureBox;
+        private System.Windows.Forms.ToolStripMenuItem importImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem playToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadImageToolStripMenuItem;
     }
 }
--- UoFiddler.Plugin.ConverterMultiTextPlugin/Forms/AnimationVDForm.cs
+++ UoFiddler.Plugin.ConverterMultiTextPlugin/Forms/AnimationVDForm.cs
@@ -14,205 +14,225 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace UoFiddler.Plugin.ConverterMultiTextPlugin.Forms
 {
     public partial class AnimationVDForm : Form
     {
-        private List<Bitmap> frames;
-        private int currentFrameIndex;
-        private Timer animationTimer;
-        private ListView listViewVD;
-        private TreeView animationTreeView;
-        private PictureBox pictureBox;
-        private TextBox textBoxInfo;
+        private List<Image> images = new List<Image>(10);
+        private int animationSpeed = 500; // Standardgeschwindigkeit in Millisekunden
+        private CancellationTokenSource cancellationTokenSource;
+        private bool isPlaying = false; // Flag, um festzustellen, ob die Animation aktiv ist
 
         public AnimationVDForm()
         {
             InitializeComponent();
-            frames = new List<Bitmap>();
-            animationTimer = new Timer();
-            animationTimer.Interval = 100; // Animation speed in milliseconds
-            animationTimer.Tick += AnimationTimer_Tick;
-
-            // Add a button to load .vd files
-            Button loadButton = new Button
+            for (int i = 0; i < 10; i++)
             {
-                Text = "Load .vd File",
-                Location = new Point(10, 10)
-            };
-            loadButton.Click += LoadButton_Click;
-            Controls.Add(loadButton);
+                images.Add(null); // Initialisiere die Liste mit null-Werten
+            }
 
-            // Add a ListView to display frames
-            listViewVD = new ListView
-            {
-                Name = "listViewVD",
-                Location = new Point(10, 50),
-                Size = new Size(200, 300),
-                View = View.List
-            };
-            listViewVD.SelectedIndexChanged += ListViewVD_SelectedIndexChanged;
-            Controls.Add(listViewVD);
+            checkedListBoxAminID.SetItemChecked(0, true); // Erste Checkbox beim Start aktivieren
 
-            // Add a TreeView to display animations
-            animationTreeView = new TreeView
-            {
-                Name = "animationTreeView",
-                Location = new Point(220, 50),
-                Size = new Size(200, 300)
-            };
-            animationTreeView.AfterSelect += AnimationTreeView_AfterSelect;
-            Controls.Add(animationTreeView);
+            // Setze TrackBar auf den Standardwert (Mitte)
+            trackBarSpeedAmin.Value = 3;
 
-            // Add a PictureBox to display the selected frame
-            pictureBox = new PictureBox
-            {
-                Name = "pictureBox",
-                Location = new Point(440, 50),
-                Size = new Size(300, 300),
-                BorderStyle = BorderStyle.FixedSingle
-            };
-            Controls.Add(pictureBox);
+            // Initialisiere das Label mit der Standardgeschwindigkeit
+            UpdateLabelSpeed();
 
-            // Add a TextBox to display information
-            textBoxInfo = new TextBox
-            {
-                Name = "textBoxInfo",
-                Location = new Point(10, 360),
-                Size = new Size(730, 50),
-                Multiline = true,
-                ReadOnly = true
-            };
-            Controls.Add(textBoxInfo);
-
-            // Add a button to save frames
-            Button saveButton = new Button
-            {
-                Text = "Save Frames",
-                Location = new Point(120, 10)
-            };
-            saveButton.Click += SaveButton_Click;
-            Controls.Add(saveButton);
+            // Event-Handler abonnieren
+            trackBarSpeedAmin.Scroll += TrackBarSpeedAmin_Scroll;
+            checkedListBoxAminID.ItemCheck += CheckedListBoxAminID_ItemCheck; // Event-Handler für CheckedListBox            
         }
 
-        private void LoadButton_Click(object sender, EventArgs e)
+        private void CheckedListBoxAminID_ItemCheck(object sender, ItemCheckEventArgs e)
         {
-            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            // Zeige das Bild nur an, wenn die Animation nicht läuft
+            if (!isPlaying && e.NewValue == CheckState.Checked)
             {
-                openFileDialog.Filter = "VD files (*.vd)|*.vd|All files (*.*)|*.*";
+                // Deaktiviere alle anderen Checkboxen
+                for (int i = 0; i < checkedListBoxAminID.Items.Count; i++)
+                {
+                    if (i != e.Index)
+                    {
+                        checkedListBoxAminID.SetItemChecked(i, false);
+                    }
+                }
+
+                var selectedIndex = e.Index;
+
+                if (selectedIndex >= 0 && selectedIndex < images.Count && images[selectedIndex] != null)
+                {
+                    pictureBoxAminImage.Image = images[selectedIndex];
+                    pictureBoxAminImage.Refresh();
+                }
+                else
+                {
+                    pictureBoxAminImage.Image = null;
+                }
+            }
+            else if (isPlaying)
+            {
+                e.NewValue = e.CurrentValue; // Verhindert Änderungen, wenn die Animation läuft
+            }
+        }
+
+        private void btLoadAminID_Click(object sender, EventArgs e)
+        {
+            if (checkedListBoxAminID.SelectedIndex == -1)
+            {
+                checkedListBoxAminID.SelectedIndex = 0; // Setze den SelectedIndex auf 0, falls keiner ausgewählt ist
+            }
+
+            if (checkedListBoxAminID.SelectedIndex >= 0 && checkedListBoxAminID.SelectedIndex < 10)
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
-                    LoadVDFile(openFileDialog.FileName);
+                    Image img = Image.FromFile(openFileDialog.FileName);
+                    images[checkedListBoxAminID.SelectedIndex] = img;
+
+                    // Zeige das geladene Bild sofort an, wenn die Animation nicht aktiv ist
+                    if (!isPlaying)
+                    {
+                        pictureBoxAminImage.Image = img;
+                        pictureBoxAminImage.Refresh();
+                    }
                 }
             }
         }
 
-        private void LoadVDFile(string filePath)
+        private async void btPlayAminID_Click(object sender, EventArgs e)
         {
-            int width = 0;
-            int height = 0;
-            frames.Clear();
-            listViewVD.Items.Clear();
-            animationTreeView.Nodes.Clear();
+            isPlaying = true; // Setze das Flag, dass die Animation läuft
+            cancellationTokenSource = new CancellationTokenSource();
+            var token = cancellationTokenSource.Token;
+
             try
             {
-                using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open)))
+                await Task.Run(async () =>
                 {
-                    // Read the header of the VD file
-                    if (reader.BaseStream.Length < sizeof(int))
+                    do
                     {
-                        throw new EndOfStreamException("Unexpected end of file while reading frame count.");
-                    }
-
-                    int frameCount = reader.ReadInt32();
-                    for (int i = 0; i < frameCount; i++)
-                    {
-                        if (reader.BaseStream.Length - reader.BaseStream.Position < sizeof(int) * 2)
+                        foreach (var img in images)
                         {
-                            throw new EndOfStreamException("Unexpected end of file while reading frame dimensions.");
-                        }
-
-                        width = reader.ReadInt32();
-                        height = reader.ReadInt32();
-
-                        if (reader.BaseStream.Length - reader.BaseStream.Position < width * height)
-                        {
-                            throw new EndOfStreamException("Unexpected end of file while reading frame data.");
-                        }
-
-                        byte[] imageData = reader.ReadBytes(width * height);
-                        Bitmap frame = new Bitmap(width, height);
-                        for (int y = 0; y < height; y++)
-                        {
-                            for (int x = 0; x < width; x++)
+                            if (img != null)
                             {
-                                byte pixelValue = imageData[y * width + x];
-                                frame.SetPixel(x, y, Color.FromArgb(pixelValue, pixelValue, pixelValue));
+                                // Überprüfen, ob die PictureBox noch gültig ist, bevor versucht wird, sie zu aktualisieren
+                                if (pictureBoxAminImage != null && !pictureBoxAminImage.IsDisposed && pictureBoxAminImage.IsHandleCreated)
+                                {
+                                    pictureBoxAminImage.Invoke(new Action(() =>
+                                    {
+                                        if (!pictureBoxAminImage.IsDisposed) // Nochmalige Prüfung
+                                        {
+                                            pictureBoxAminImage.Image = img;
+                                            pictureBoxAminImage.Refresh();
+                                        }
+                                    }));
+                                }
+                                await Task.Delay(animationSpeed, token); // Wartezeit zwischen den Bildern
                             }
                         }
-                        frames.Add(frame);
-                        listViewVD.Items.Add($"Frame {i + 1}");
-                        animationTreeView.Nodes.Add($"Frame {i + 1}");
-                    }
-                    DisplayAnimationInfo(frameCount, width, height);
-                }
+                    } while (checkBoxLoop.Checked && !token.IsCancellationRequested);
+                }, token);
             }
-            catch (EndOfStreamException ex)
+            catch (TaskCanceledException)
             {
-                MessageBox.Show($"File format error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // Aufgabe wurde abgebrochen, keine Aktion erforderlich
             }
-            catch (Exception ex)
+            finally
             {
-                MessageBox.Show($"Error loading VD file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                isPlaying = false; // Animation beendet
             }
         }
 
-        private void ListViewVD_SelectedIndexChanged(object sender, EventArgs e)
+        private void TrackBarSpeedAmin_Scroll(object sender, EventArgs e)
         {
-            ListView listView = sender as ListView;
-            if (listView.SelectedIndices.Count > 0 && listView.SelectedIndices[0] < frames.Count)
+            // Berechnung der Geschwindigkeit basierend auf der Position der TrackBar
+            switch (trackBarSpeedAmin.Value)
             {
-                pictureBox.Image = frames[listView.SelectedIndices[0]];
+                case 1:
+                    animationSpeed = 2000; // Langsamste Geschwindigkeit
+                    break;
+                case 2:
+                    animationSpeed = 1000; // Langsam
+                    break;
+                case 3:
+                    animationSpeed = 500; // Standardgeschwindigkeit
+                    break;
+                case 4:
+                    animationSpeed = 100; // Schneller
+                    break;
+                case 5:
+                    animationSpeed = 25; // Schnellste Geschwindigkeit
+                    break;
+                default:
+                    animationSpeed = 500; // Standardgeschwindigkeit
+                    break;
+            }
+
+            // Aktualisiere das Label mit der aktuellen Geschwindigkeit
+            UpdateLabelSpeed();
+        }
+
+        private void UpdateLabelSpeed()
+        {
+            labelSpeed.Text = $"Speed: {animationSpeed} ms";
+        }
+
+        private void btStopAminID_Click(object sender, EventArgs e)
+        {
+            StopAnimation(); // Stoppt die Animation sicher
+        }
+
+        private async void AnimationVDForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Wenn die Animation läuft, zuerst stoppen
+            if (isPlaying)
+            {
+                e.Cancel = true; // Schließen abbrechen
+                await Task.Run(() => StopAnimation()); // Animation stoppen
+                this.Close(); // Form erneut schließen
             }
         }
 
-        private void AnimationTreeView_AfterSelect(object sender, TreeViewEventArgs e)
+        private void StopAnimation()
         {
-            int selectedIndex = e.Node.Index;
-            if (selectedIndex != -1 && selectedIndex < frames.Count)
+            if (cancellationTokenSource != null)
             {
-                pictureBox.Image = frames[selectedIndex];
+                cancellationTokenSource.Cancel();
+                isPlaying = false;
             }
         }
 
-        private void AnimationTimer_Tick(object sender, EventArgs e)
+        private void importImageToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (frames.Count > 0)
+            if (Clipboard.ContainsImage())
             {
-                currentFrameIndex = (currentFrameIndex + 1) % frames.Count;
-                pictureBox.Image = frames[currentFrameIndex];
-            }
-        }
+                Image img = Clipboard.GetImage();
 
-        private void DisplayAnimationInfo(int frameCount, int width, int height)
-        {
-            textBoxInfo.Text = $"Frames: {frameCount}\nWidth: {width}\nHeight: {height}";
-        }
-
-        private void SaveButton_Click(object sender, EventArgs e)
-        {
-            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
-            {
-                if (folderDialog.ShowDialog() == DialogResult.OK)
+                if (checkedListBoxAminID.SelectedIndex >= 0 && checkedListBoxAminID.SelectedIndex < images.Count)
                 {
-                    string folderPath = folderDialog.SelectedPath;
-                    for (int i = 0; i < frames.Count; i++)
+                    images[checkedListBoxAminID.SelectedIndex] = img;
+
+                    // Zeige das geladene Bild sofort an, wenn die Animation nicht aktiv ist
+                    if (!isPlaying)
                     {
-                        frames[i].Save(Path.Combine(folderPath, $"frame_{i}.png"), System.Drawing.Imaging.ImageFormat.Png);
+                        pictureBoxAminImage.Image = img;
+                        pictureBoxAminImage.Refresh();
                     }
-                    MessageBox.Show("Frames saved successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
+                else
+                {
+                    MessageBox.Show("Bitte wählen Sie eine gültige Checkbox aus.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Kein Bild in der Zwischenablage gefunden.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
